6e374e315df502b2b6fab7018a1aa6456d3fffac
Purushotham Reddy Jakka <jakka.purushotham@gmail.com>
2024-06-14 22:23:18 +0700

    Refactor code

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,4 @@
-function S4() {
-  return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
-};
-
-function guid() {
-  return (S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4());
-};
+import { v4 as uuidv4 } from 'uuid';
 
 interface Model {
   id: string;
@@ -14,79 +8,58 @@ interface Model {
 class LocalStorageService {
   private name: string;
   private records: string[];
-  private localStorage = () => {
-    return localStorage;
-  };
-
-  private static jsonData(data: string) {
-    return data && JSON.parse(data);
-  };
+  private localStorage = window.localStorage;
 
   constructor(name: string) {
     if (!name) {
-      throw new Error("Namespace is required.");
+      throw new Error('Namespace is required.');
     }
     this.name = name;
-    const store = this.localStorage().getItem(this.name);
-    this.records = (store && store.split(",")) || [];
+    const store = this.localStorage.getItem(this.name);
+    this.records = (store && store.split(',')) || [];
   }
 
   private save() {
-    this.localStorage().setItem(this.name, this.records.join(","));
-  };
+    this.localStorage.setItem(this.name, this.records.join(','));
+  }
 
   public get(id: string) {
-    const data = this.localStorage().getItem(this.name + "-" + id);
-    if (!data) {
-      return null;
-    } else {
-      return LocalStorageService.jsonData(data);
-    }
-  };
+    const data = this.localStorage.getItem(`${this.name}-${id}`);
+    return data ? JSON.parse(data) : null;
+  }
 
   public getAll() {
-    return this.records.map((recordId) => {
-      const record = this.localStorage().getItem(this.name + "-" + recordId);
-      if (record) {
-        return LocalStorageService.jsonData(record);
-      } else {
-        return null;
-      }
-    })
+    return this.records.map((recordId) => this.get(recordId));
   }
 
   public create(model: Model) {
     if (!model.id) {
-      model.id = guid();
+      model.id = uuidv4();
     }
-    this.localStorage().setItem(this.name + "-" + model.id, JSON.stringify(model));
-    this.records.push(model.id.toString());
+    this.localStorage.setItem(`${this.name}-${model.id}`, JSON.stringify(model));
+    this.records.push(model.id);
     this.save();
     return this.get(model.id);
   }
 
   public update(model: Model) {
-    this.localStorage().setItem(this.name + "-" + model.id, JSON.stringify(model));
-    if (!this.records.find(element => element === model.id.toString())) {
-      this.records.push(model.id.toString());
+    this.localStorage.setItem(`${this.name}-${model.id}`, JSON.stringify(model));
+    if (!this.records.includes(model.id)) {
+      this.records.push(model.id);
       this.save();
     }
     return this.get(model.id);
   }
 
   public delete(id: string) {
-    this.localStorage().removeItem(this.name + "-" + id);
-    this.records = this.records.filter((recordId) => {
-      return recordId !== id.toString();
-    });
+    this.localStorage.removeItem(`${this.name}-${id}`);
+    this.records = this.records.filter((recordId) => recordId !== id);
     this.save();
     return true;
-  };
+  }
 
   public deleteAll() {
-    this.records.forEach((recordId) => {
-      this.localStorage().removeItem(this.name + "-" + recordId)
-    });
+    this.records.forEach((recordId) => this.localStorage.removeItem(`${this.name}-${recordId}`));
     this.records = [];
     this.save();
     return true;
